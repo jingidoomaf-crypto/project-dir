28cee4ce1431e9a2a25d772249491206509ef3ae
jingidoomaf <jingidoomaf@gmail.com>
2025-10-21 22:48:48 +0700

    Updated register page and connected routes

--- app.py
+++ app.py
@@ -3,9 +3,20 @@ import os
 
 app = Flask(__name__)
 
+# Home Page
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Login Page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Register Page
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5055))
