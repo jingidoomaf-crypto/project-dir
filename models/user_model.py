ff0c3c246fe5c5fdf1f36fc65098dba4af6c0a2a
Karolle Ona <onakarolle21@gmail.com>
2025-11-15 02:57:48 +0800

    added changes in user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -19,4 +19,4 @@ class User(db.Model):
     updated_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
     def __repr__(self):
-        return f"<User {self.uname}>"
+        return f"<User {self.email}>"
