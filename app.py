3904b64ab209a261767aebcc03d6277a244cc8be
jingidoomaf <jingidoomaf@gmail.com>
2025-11-24 10:04:41 +0700

    Update app.py and add create_db.py

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 from config import Config
 from models.db import db
-
+from flask_migrate import Migrate
 import os
 
 load_dotenv()
 app = Flask(__name__)
 app.config.from_object(Config)
-
+db.init_app(app)
+migrate = Migrate(app, db)
 
 db.init_app(app)
 
